30b32061476334b1deccf39f51a1c3ccf5780307
Addin Munawwar <moonawar@protonmail.com>
2025-01-25 14:14:34 +0700

    feat: webgl initialization

--- src/ui/MainCanvas.svelte
+++ src/ui/MainCanvas.svelte
@@ -1,18 +1,40 @@
-<script>
-    // Canvas Size Initialization
+<script lang="ts">
     import { onMount } from "svelte";
 
+    let canvas : HTMLCanvasElement | null = null;
+    let gl : WebGL2RenderingContext | null = null;
     let containerWidth = 0, containerHeight = 0;
-    let canvasWidth = 0, canvasHeight = 0;
+
+    const initWebGL = () => {
+        if (!canvas) {
+            console.error("Canvas not found");
+            return;
+        }
+
+        gl = canvas.getContext("webgl2");
+        if (!gl) {
+            console.error("WebGL 2 is not supported");
+            return;
+        }
+    }
+
+    const resizeCanvas = () => {
+        if (!canvas) {
+            console.error("Canvas not found");
+            return;
+        }
+
+        canvas.width = containerWidth;
+        canvas.height = containerHeight;
+    }
 
     onMount(() => {
-        canvasWidth = containerWidth;
-        canvasHeight = containerHeight;
+        resizeCanvas();
     });
 </script>
 
 <div class="flex-1 h-full overflow-hidden bg-dark-500" id="main-canvas-container"
     bind:clientWidth={containerWidth} bind:clientHeight={containerHeight}
 > 
-    <canvas id="main-canvas" width={canvasWidth} height={canvasHeight}></canvas>
+    <canvas id="main-canvas" bind:this={canvas}></canvas>
 </div>
